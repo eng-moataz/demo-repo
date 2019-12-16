65ffe63103f68a2f7e921ee997ea3d2c15e56dfa
Gaber <moagaber@4c32759784a7.ant.amazon.com>
2019-12-16 19:30:45 +0000

    reverting back to /var/log/sample1.log

--- application.py
+++ application.py
@@ -10,7 +10,7 @@ logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
 # Handler 
-LOG_FILE = '/dev/stdout'
+LOG_FILE = 'var/log/sample1.log'
 handler = logging.handlers.RotatingFileHandler(LOG_FILE, maxBytes=1048576, backupCount=5)
 handler.setLevel(logging.INFO)
 
@@ -207,7 +207,7 @@ def application(environ, start_response):
     headers = [('Content-type', 'text/html')]
 
     start_response(status, headers)
-    logger.info("%s %s %s %s", 'python', status,environ['REQUEST_METHOD'],environ['PATH_INFO'])
+    logger.info("%s %s %s %s",'python', status,environ['REQUEST_METHOD'],environ['PATH_INFO'])
     return [response]
 
 class ThreadingWSGIServer(ThreadingMixIn, WSGIServer): 
