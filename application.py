2fc6d99dedf63c178711c200bc68948c4bd3e047
Gaber <moagaber@4c32759784a7.ant.amazon.com>
2019-12-17 15:36:28 +0000

    Fix!

--- application.py
+++ application.py
@@ -10,7 +10,7 @@ logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
 # Handler 
-LOG_FILE = '/dev/stdout'
+LOG_FILE = '/var/log/sample1.log'
 handler = logging.handlers.RotatingFileHandler(LOG_FILE, maxBytes=1048576, backupCount=5)
 handler.setLevel(logging.INFO)
 
@@ -59,7 +59,7 @@ welcome = """
     text-shadow: #fff 0px 0px 25px;
   }
   a {
-    color: green;
+    color: blue;
   }
   .textColumn, .linksColumn {
     padding: 2em;
